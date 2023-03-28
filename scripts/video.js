775b11add8910f4bd70dee8a8ff2fdf569c4993a
Asilbek <asilbekpro37@gmail.com>
2023-03-28 23:40:15 +0500

    first commit

---
+++ scripts/video.js
@@ -0,0 +1,36 @@
+const videoOverlay = document.querySelector(".video-overlay"),
+    watchBtn = document.querySelector(".watch_video-btn"),
+    closeBtn = document.querySelector(".video-close"),
+    body = document.querySelector("body"),
+    videoBlock = document.querySelector(".video-overlay .video");
+
+let overlayShow = function () {
+    videoBlock.insertAdjacentHTML(
+        "afterbegin",
+        `<iframe width="800" height="475"
+            src="https://www.youtube.com/embed/EDIFIBFRoXA?rel=0&modestbranding=1&autoplay=1"
+            frameborder="0" allowfullscreen>
+        </iframe>`
+    );
+
+    videoOverlay.classList.remove("hidden");
+    body.style.overflowY = "hidden";
+};
+
+let overlayHide = function () {
+    videoOverlay.classList.add("hidden");
+    body.style.overflowY = "scroll";
+
+    let videoIframe = videoBlock.querySelector("iframe");
+
+    videoBlock.removeChild(videoIframe);
+};
+
+watchBtn.addEventListener("click", overlayShow);
+
+closeBtn.addEventListener("click", overlayHide);
+
+videoOverlay.addEventListener("click", (e) => {
+    e.target == videoOverlay ? overlayHide() : null;
+});
+
